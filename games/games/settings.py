a3106080fe3fd0165ba9cc8f82f906b5cb77d2dc
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2015-11-17 00:59:24 +0200

    Changed database to postgres.

--- games/games/settings.py
+++ games/games/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'games.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'games',
+        'USER': 'django_games',
+        'PASSWORD': 'data4tichu4hack4dolphin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
