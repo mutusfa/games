19a6f49df63a624331eac992db4fa49202228085
Julius Juodagalvis <julius.juodagalvis@gmail.com>
2015-11-19 01:48:30 +0200

    Changed from colours to symbols.

--- games/tichu/constants.py
+++ games/tichu/constants.py
@@ -1,4 +1,4 @@
-COLOURS = (("R", "Red"), ("G", "Green"), ("B", "Blue"), ("L", "Black"))
+COLOURS = (("J", "Jack"), ("S", "Sword"), ("P", "Pagoda"), ("T", "Star"))
 NO_PLAYERS = 4
 GRAND_TICHU = 8
 MIN_ROW_LENGTH = 5
